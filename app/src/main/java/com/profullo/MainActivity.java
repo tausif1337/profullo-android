4bf516fff770f68f235cabb34c803b43019ce743
Tausif <tausif.iub@gmail.com>
2020-01-16 09:57:58 +0600

    Android Application for the website https://www.profullo.com/

---
+++ app/src/main/java/com/profullo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.profullo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webViewId);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.profullo.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
